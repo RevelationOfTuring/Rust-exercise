fd35c381f06c69e2a25ad0e1b53c2bed4cf11cd7
MichaelWang <guanyi.wang@okcoin.net>
2019-12-31 10:48:58 +0800

    tmp save

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,55 @@
-//Add an implementation of the fmt::Display trait for the Color struct above so that the output displays as:
-//
-//RGB (128, 255, 90) 0x80FF5A
-//RGB (0, 3, 254) 0x0003FE
-//RGB (0, 0, 0) 0x000000
+/*
+Activity
+Recap: Add the fmt::Display trait to the Matrix struct in the above example, so that if you switch from printing the debug format {:?} to the display format {}, you see the following output:
 
-//Two hints if you get stuck:
-//You may need to list each color more than once,
-//You can pad with zeros to a width of 2 with :02.
+( 1.1 1.2 )
+( 2.1 2.2 )
+You may want to refer back to the example for print display.
 
+Add a transpose function using the reverse function as a template, which accepts a matrix as an argument, and returns a matrix in which two elements have been swapped. For example:
+
+println!("Matrix:\n{}", matrix);
+println!("Transpose:\n{}", transpose(matrix));
+results in the output:
+
+Matrix:
+( 1.1 1.2 )
+( 2.1 2.2 )
+Transpose:
+( 1.1 2.1 )
+( 1.2 2.2 )
+*/
 use std::fmt::{Display, Formatter, Error};
 
-struct City {
-    name: &'static str,
-    lat: f32,
-    lon: f32,
-}
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables
+    let (integer, boolean) = pair;
 
-impl Display for City {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
-        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
-
-        write!(f, "{}:{:.3}°{} {:.3}°{}",
-               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
-    }
+    (boolean, integer)
 }
 
 #[derive(Debug)]
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
+struct Matrix(f32, f32, f32, f32);
+
+// Activity1
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
 }
 
-// attention!!!!!
-impl Display for Color {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        write!(f, "RGB ({0}, {1}, {2}) 0x{0:02X}{1:02X}{2:02X}", self.red, self.green, self.blue)
-    }
+// Activity2
+fn transpose(matrix: &Matrix) -> Matrix {
+    Matrix { 0: matrix.0, 1: matrix.2, 2: matrix.1, 3: matrix.3 }
 }
 
 fn main() {
-    for city in [
-        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
-        City { name: "Oslo", lat: 59.95, lon: 10.75 },
-        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
-    ].iter() {
-        println!("{}", *city);
-    }
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    // Activity1
+    println!("{:?}", matrix);
+    println!("{}", matrix);
 
-    for color in [
-        Color { red: 128, green: 255, blue: 90 },
-        Color { red: 0, green: 3, blue: 254 },
-        Color { red: 0, green: 0, blue: 0 },
-    ].iter() {
-//        println!("{:?}", *color);
-        println!("{}", *color);
-    }
+    // Activity2
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(&matrix));
 }
