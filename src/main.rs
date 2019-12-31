d9a6f9838ab6ce4b60d5948490fcdbab14f456d8
MichaelWang <guanyi.wang@okcoin.net>
2019-12-31 18:25:24 +0800

    tmp save

--- src/main.rs
+++ src/main.rs
@@ -157,4 +157,45 @@ mod tests {
 // 使用 Into trait 通常要求指明要转换到的类型，因为编译器大多数时候不能推断它。不
 // 过考虑到我们免费获得了 Into，这点代价不值一提。
     }
+
+    // ToString和FromStr
+    #[test]
+    fn test_ToString_and_FromStr() {
+        // 要把任何类型转换成 String，只需要实现那个类型的 ToString trait。
+        struct Circle {
+            radius: i32
+        }
+
+        impl std::string::ToString for Circle {
+            fn to_string(&self) -> String {
+                format!("Circle of radius {:?}", self.radius)
+            }
+        }
+
+        let c = Circle { radius: 12 };
+        println!("{}", c.to_string());
+
+        // 我们经常需要把字符串转成数字。完成这项工作的标准手段是用 parse 函数。
+        // 我们得提供要转换到的类型，这可以通过不使用类型推断，或者用 “涡轮鱼” 语法（turbo fish，<>）实现。
+        // 只要对目标类型实现了 FromStr trait，就可以用 parse 把字符串转换成目标类型。
+        // 标准库中已经给无数种类型实现了 FromStr。如果要转换到用户定义类型，只要手动实现 FromStr 就行。
+
+        // 需要手动提供要转换成的类型
+        let parsed: i32 = "1024".parse().unwrap();
+        println!("{}", parsed);
+
+        // 实现FromStr trait
+        use std::num::ParseIntError;
+        impl std::str::FromStr for Circle {
+            type Err = ParseIntError;
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let strs: Vec<&str> = s.trim().split(' ').collect();
+                let r = strs[3].parse::<i32>()?;
+                Ok(Circle { radius: r })
+            }
+        }
+
+        let parsed_circle: Circle = "Circle of radius 1025".parse().unwrap();
+        println!("{}", parsed_circle.radius);
+    }
 }
